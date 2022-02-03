cd607354c43902288f3875406608e4482ad9484e
Darcy <darcy.lauder@shaw.ca>
2022-02-03 15:23:17 -0700

    additional commenting

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-  
-  const CHAR_LIMIT = 140;
 
+  // varaible declarations
+  const CHAR_LIMIT = 140;
   $('.counter').text(CHAR_LIMIT);
   $('#varCharsRemaining').text(CHAR_LIMIT);
 
+  // counter logic
   $('#tweet_text').keyup(function() {
     let tweetLength = CHAR_LIMIT - $(this).val().length;
     $('.counter').text(tweetLength);
@@ -15,6 +16,7 @@ $(document).ready(function() {
     }
   });
 
+  // auto grow the text input
   $('#tweet_text').on('input', function() {
     let scroll_height = $('#tweet_text').get(0).scrollHeight;
     $('#tweet_text').css('height', scroll_height + 'px');
